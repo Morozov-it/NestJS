31e9fd498592312d59ea10903b20b51a4ff4c7fb
Morozov Grigory <morozov.it2020@gmail.com>
2022-03-31 14:39:57 +0300

    first commit

---
+++ src/main.ts
@@ -0,0 +1,11 @@
+import { NestFactory } from "@nestjs/core";
+
+
+async function start() {
+    const PORT = process.env.PORT || 5000;
+    const app = await NestFactory.create()
+
+    await app.listen(PORT, () => console.log(`server started on port ${PORT}`))
+
+}
+start()
